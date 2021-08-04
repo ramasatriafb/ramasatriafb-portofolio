de5c3d327e83bcfc6fdd446f9b07c2132a829a3d
Rama Firmansyah <ramasatriafb@gmail.com>
2021-08-04 14:04:24 +0700

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ export default function About() {
                     <Col xs={12}md={6} className="mx-auto">  
                     <h2 className="text-start mt-5 tagPro2 ">Why  I started focusing <p style={{color: "#114A90"}}>Front End Dev ? </p></h2>
                     <p className="paragraph-pro text-start me-5">
-                    My interest in application design started in 2020 where I handled projects that required me to learn UI and UX and develop front end applications. Since this experience I began to practice to deepen the field of Front End. With several projects that I have worked on related to the frontend and my great interest in user interfaces and problem solving through application design. I convinced myself to focus on Front End Developer where previously I was a full stack developer programmer.
+                    My interest in application design started in 2020 where I handled projects that required me to learn UI and UX and develop front end applications. Since this experience I began to practice to deepen the field of Front End. With several projects that I have worked on related to the frontend and my great interest in user interfaces and problem solving through application design. I convinced myself to focus on Front End Developer where previously I was a full stack programmer.
                     </p>
                     </Col>
                         
